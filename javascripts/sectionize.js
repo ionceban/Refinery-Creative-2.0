ad9c0cccae9fdba0546f68407aed7520fd95ccf0
fk1blow <hedphug@gmail.com>
2011-09-16 20:41:01 +0300

    Edited javascripts/sectionize.js via GitHub

--- javascripts/sectionize.js
+++ javascripts/sectionize.js
@@ -3,173 +3,95 @@
  * - toggles the main items from #work section
  */
 var Sectionize = (function() {
-	var DEFAULT_TUNE 	= 95;
-	var container 		= $('#dummy-work-inner');
-	
-	var current_margin 	= 0;
-	var current_elem 	= null;
-	var current_slide 	= null;
-	var is_animating 	= false;
-	
-	
-	var getOffset = function() {
-		var elem_offset = $(current_elem).parent().offset().top;
-		return elem_offset - DEFAULT_TUNE;
+	var defaults = {
+		topGutter: 69
 	}
 	
-	var setCurrentSlide = function(elem) {
-		current_slide = elem;
-	}
+	var currentElement = null;
 	
-	var closeCurrent = function(callback) {
-		is_animating = true;
-		
-		$(container).animate({
-			'margin-top': 0
-		}, 250, function() {
-			is_animating = false;
-			window.location = '#!/'
-			callback.call(callback);
-		});
-		
-		current_margin = 0;
-		setCurrentSlide(null);
-	}
 	
-	var closeAndOpen = function(callback) {
-		current_margin += getOffset();
-		is_animating = true;
-		
-		$(container).animate({
-			'margin-top': '-' + current_margin + 'px'
-		}, 250, function() {
-			is_animating = false;
-			callback.call(callback);
-		});
-		
-		setCurrentSlide(current_elem);
-	}
-	
-	var moveTo = function(elem) {
-		current_elem = elem;
-		if(current_elem == current_slide) {
-			closeCurrent(function() {
-				Callbacks.after({ elem: current_elem, state: 'closed' })
-			});
-		} else {
-			closeAndOpen(function() {
-				Callbacks.after({ elem: current_elem, state: 'open' })
-			});
+	/**
+	 * Get the offset of an elemenet
+	 */
+	var getOffset = function(elem) {
+		if($(elem)) {
+			return $(elem).offset().top - defaults.topGutter;
 		}
+		return null;
 	}
 	
-	var resetSections = function(cb) {
-		setCurrentSlide(null);
-		current_margin = 0;
-		
-		$(container).animate({
-			'margin-top': 0
-		}, 250, function() {
-			is_animating = false
-			closeAllSections();
-			if(cb) {
-				cb.call(this);
+	
+	/**
+	 * Moves the scroll to a desired element position
+	 */
+	var scrollTo = function(position, cb) {
+		$('body, html').animate({ scrollTop: position }, {
+			duration: 350,
+			easing: 'easeInOutQuint',
+			complete: function() {
+				if(cb)
+					cb.call(this);
 			}
-			closeAllSubcategories();
 		});
 	}
 	
 	/**
-	 * Closes all the sections left
+	 * reset the fucking scroll
 	 */
-	var closeAllSections = function() {
-		$('#dummy-work-inner').find('.work-section a.active').each(function() {
-			$(this).removeClass('active');
-		});
+	var resetScroll = function(elem) {
+		closeSection(elem);
 	}
 	
 	/**
-	 * Closes all the sections categories left displayed
-	 */ 
-	var closeAllSubcategories = function() {
-		$('#dummy-work-inner').find('.subcontent-active').attr('class', 'subcontent')
-		$('#dummy-work-inner').find('.subcontent').css({'opacity': 0})
+	 * closes a section
+	 */
+	var closeSection = function(elem) {
+		$(elem).next('.subcontent-active').hide().find('.section-dynamic-content').empty();
+		currentElement = null;
+		window.location = '#!/';
+		scrollTo(0);
 	}
 	
 	/**
-	 * Toggles the sections visibility
+	 * closes a section and opens a new one
 	 */
-	var toggleSections = function(section, state) {
-		closeAllSections();
-		
-		if(state == 'open') {
-			$(section).addClass('active');
-			toggleCategory($(section).next(), 'open');
+	var openSection = function(elem) {
+		if(currentElement) {
+			$(currentElement).next('.subcontent-active').fadeOut(250).animate(
+				{'opacity': 0},
+				{
+					easing: 'easeInOutExpo',
+					duration: 500,
+					complete: function() {
+						$(this).find('.section-dynamic-content').empty();
+						scrollTo(getOffset(elem));
+					}
+				}
+			);
 		} else {
-			$(section).removeClass('active');
-			toggleCategory($(section).next(), 'close');
+			scrollTo(getOffset(elem));
 		}
-	}
-	
-	/**
-	 * Toggles sections categories visibility
-	 */
-	var toggleCategory = function(category, state) {
-		// TEMP
-		return;
-		var elem_class = (state == 'open') ? 'subcontent-active' : 'subcontent';
-		$(category).attr('class', elem_class);
-	}
-	
-	
-	/**
-	 * Callbacks
-	 */
-	var Callbacks = {
-		before: function() {
-			closeAllSubcategories();
-			if(Options.before)
-				Options.before.call();
-		},
 		
-		after: function(options) {
-			toggleSections(options.elem, options.state);
-			if(Options.after) {
-				Options.after.call(Options.after, options.elem, options.state);
-			}
-		},
-		
-		clear: function() {
-			Options.before = null;
-			Options.after = null;
-		}
+		currentElement = elem;
 	}
 	
-	/**
-	 * The main options object
-	 * contains only(for the moment) the callbacks
-	 */
-	var Options = {
-		before: null,
-		after: null
-	};
+	var scrollToElement = function(elem) {
+		var elemOffset = getOffset(elem);
+		//if(currentElement == elem && ($(currentElement).attr('href') == window.location.hash)) {
+		//	closeSection(elem);
+		//} else {
+			openSection(elem);
+		//}
+	}
 	
-	/**
-	 * Public interface
-	 */
+	// Public
 	return {
 		toggle: function(elem, _options) {
-			jQuery.extend(Options, _options);
-			Callbacks.before();
-			moveTo(elem);
+			scrollToElement(elem);
 		},
 		
-		reset: function(callback) {
-			resetSections(callback);
-		},
-		
-		isAnimating: function() {
-			return is_animating;
+		reset: function(elem) {
+			resetScroll(elem);
 		}
 	}
 })();
